96e17a8c9f8562e47c7c0059af0a112154a8711a
desimorenoh <desimorenoh@gmail.com>
2021-11-02 15:35:26 +0100

    jquery

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    let tasks = [];
+
+    $('#submit').on("click", function() {
+        let newTask = $('#newTask').val();
+
+        if (newTask !== "") {
+
+            tasks.push(newTask);
+
+            $('#taskList').empty()
+
+            tasks.forEach(t =>
+                $('#taskList').append(
+                    `<div class="task">${t}</div>`
+                )
+            );
+        }
+
+    })
+
+    $('#clearButton').on("click", function() {
+        $('.task').remove();
+        tasks.length = [];
+    })
+});
